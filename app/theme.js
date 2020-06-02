df0dd54140ee01853709c6809d2f1145d56f1079
Srikanth Pisharody <pisharodysrikanth@gmail.com>
2020-06-02 19:21:19 +0530

    add global styles for css baseline

--- app/theme.js
+++ app/theme.js
@@ -3,9 +3,26 @@ import purple from '@material-ui/core/colors/purple';
 
 const theme = createMuiTheme({
     palette: {
-        primary: purple
+        primary: purple,
+        background: {
+            default: '#eee'
+        }
     },
-    boxShadow: '0 4px 20px 0 rgba(0,0,0,.14), 0 7px 10px -5px rgba(156,39,176,.4)'
+    boxShadow: '0 4px 20px 0 rgba(0,0,0,.14), 0 7px 10px -5px rgba(156,39,176,.4)',
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                html: {
+                    height: '100%',
+                    width: '100%'
+                },
+                body: {
+                    height: '100%',
+                    width: '100%'
+                }
+            },
+        }
+    }
 });
 
 export default theme;
